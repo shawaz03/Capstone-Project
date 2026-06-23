e6a9510e9efee259ebee0534e5cd6ea2ee01d0fd
shawaz03 <shawaz03@users.noreply.github.com>
2026-06-23 13:58:13 +0530

    Fix second batch of grading items (Tasks 14, 15, 18, 20, 21, 28)

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -16,14 +16,28 @@ def initiate():
     car_model_data = [
         {"name": "Corolla", "type": "SEDAN", "year": 2023, "car_make": car_make_instances[0], "dealer_id": 1},
         {"name": "Camry", "type": "SEDAN", "year": 2023, "car_make": car_make_instances[0], "dealer_id": 1},
+        {"name": "RAV4", "type": "SUV", "year": 2023, "car_make": car_make_instances[0], "dealer_id": 1},
+        {"name": "Prius", "type": "SEDAN", "year": 2023, "car_make": car_make_instances[0], "dealer_id": 1},
+        
         {"name": "Civic", "type": "SEDAN", "year": 2023, "car_make": car_make_instances[1], "dealer_id": 1},
         {"name": "Accord", "type": "SEDAN", "year": 2023, "car_make": car_make_instances[1], "dealer_id": 1},
+        {"name": "CR-V", "type": "SUV", "year": 2023, "car_make": car_make_instances[1], "dealer_id": 1},
+        {"name": "Pilot", "type": "SUV", "year": 2023, "car_make": car_make_instances[1], "dealer_id": 1},
+        
         {"name": "Mustang", "type": "SPORTS", "year": 2023, "car_make": car_make_instances[2], "dealer_id": 2},
         {"name": "Explorer", "type": "SUV", "year": 2023, "car_make": car_make_instances[2], "dealer_id": 2},
+        {"name": "F-150", "type": "SUV", "year": 2023, "car_make": car_make_instances[2], "dealer_id": 2},
+        {"name": "Escape", "type": "SUV", "year": 2023, "car_make": car_make_instances[2], "dealer_id": 2},
+        
         {"name": "Altima", "type": "SEDAN", "year": 2023, "car_make": car_make_instances[3], "dealer_id": 3},
         {"name": "Pathfinder", "type": "SUV", "year": 2023, "car_make": car_make_instances[3], "dealer_id": 3},
+        {"name": "Sentra", "type": "SEDAN", "year": 2023, "car_make": car_make_instances[3], "dealer_id": 3},
+        {"name": "Rogue", "type": "SUV", "year": 2023, "car_make": car_make_instances[3], "dealer_id": 3},
+        
         {"name": "Cruze", "type": "SEDAN", "year": 2023, "car_make": car_make_instances[4], "dealer_id": 4},
-        {"name": "Equinox", "type": "SUV", "year": 2023, "car_make": car_make_instances[4], "dealer_id": 4}
+        {"name": "Equinox", "type": "SUV", "year": 2023, "car_make": car_make_instances[4], "dealer_id": 4},
+        {"name": "Silverado", "type": "SUV", "year": 2023, "car_make": car_make_instances[4], "dealer_id": 4},
+        {"name": "Malibu", "type": "SEDAN", "year": 2023, "car_make": car_make_instances[4], "dealer_id": 4}
     ]
 
     for data in car_model_data:
